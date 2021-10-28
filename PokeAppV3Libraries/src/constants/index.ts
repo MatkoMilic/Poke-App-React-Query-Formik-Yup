1e407693919638e28fe44b610b31becbe50bdbd7
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-28 13:45:49 +0200

    refactor(constants/index.ts): add index to constants

--- PokeAppV3Libraries/src/constants/index.ts
+++ PokeAppV3Libraries/src/constants/index.ts
@@ -1,1 +1,30 @@
-export {};
+import {
+  LOADING_SCREEN,
+  POKELIST_SCREEN,
+  LOGIN_SCREEN,
+  PROFILE_SCREEN,
+  SETTINGS_SCREEN,
+} from './screenNames';
+import {navigatorNames} from './navigatorNames';
+import {
+  OnboardingStackParamList,
+  MainStackParamList,
+  RootNavigatorParamsList,
+  RootNavigatorNameType,
+} from './navigatorTypes';
+
+export {
+  LOADING_SCREEN,
+  POKELIST_SCREEN,
+  LOGIN_SCREEN,
+  SETTINGS_SCREEN,
+  PROFILE_SCREEN,
+  navigatorNames,
+};
+
+export type {
+  OnboardingStackParamList,
+  MainStackParamList,
+  RootNavigatorParamsList,
+  RootNavigatorNameType,
+};
