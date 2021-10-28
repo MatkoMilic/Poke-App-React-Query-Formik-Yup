df0ccb992375f2800a4925f22ea0f6ee1a7ca2e2
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-28 13:58:35 +0200

    refactor(loadingscreen): add navigation to screen used for testing but left because it is valuable

--- PokeAppV3Libraries/src/screens/LoadingScreen/LoadingScreen.tsx
+++ PokeAppV3Libraries/src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {CompositeNavigationProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {ScreenContainer} from '../../components/ScreenContainer';
+import {
+  OnboardingStackParamList,
+  RootNavigatorParamsList,
+} from '../../constants';
 
-interface LoadingScreenProps {}
+interface LoadingScreenProps {
+  navigation: CompositeNavigationProp<
+    NativeStackNavigationProp<OnboardingStackParamList, 'LoadingScreen'>,
+    NativeStackNavigationProp<RootNavigatorParamsList>
+  >;
+}
 
-const LoadingScreen: React.FC<LoadingScreenProps> = () => {
+const LoadingScreen: React.FC<LoadingScreenProps> = ({navigation}) => {
   return (
     <ScreenContainer>
-      <Text>Welcome</Text>
+      <Text>Welcome to Loading Screen</Text>
     </ScreenContainer>
   );
 };
