db6693e8972cb43858a5110cd824620a65c877a2
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-29 15:51:51 +0200

    feat(loadingscreen.tsx): add change theme feature to loading screen. (It'll be moved to settings)

--- PokeAppV3Libraries/src/screens/LoadingScreen/LoadingScreen.tsx
+++ PokeAppV3Libraries/src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {CompositeNavigationProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useTheme, TouchableRipple, Switch} from 'react-native-paper';
 import {ScreenContainer} from '../../components/ScreenContainer';
 import {
   OnboardingStackParamList,
   RootNavigatorParamsList,
 } from '../../constants';
+import style from './style';
+import {StatusBar, PreferencesContext} from '../../components';
 
 interface LoadingScreenProps {
   navigation: CompositeNavigationProp<
@@ -15,10 +18,29 @@ interface LoadingScreenProps {
   >;
 }
 
-const LoadingScreen: React.FC<LoadingScreenProps> = ({navigation}) => {
+const LoadingScreen: React.FC<LoadingScreenProps> = ({
+  navigation,
+  children,
+}) => {
+  const theme = useTheme();
+  const {toggleTheme, isThemeDark} = React.useContext(PreferencesContext);
+
   return (
     <ScreenContainer>
-      <Text>Welcome to Loading Screen</Text>
+      <StatusBar />
+      <View style={style.titleView}>
+        <Text style={[{color: theme.colors.text}, style.titleText]}>
+          Welcome to Loading Screen
+        </Text>
+        <TouchableRipple>
+          <Switch
+            style={[{backgroundColor: theme.colors.text}, style.switchElement]}
+            color={theme.colors.text}
+            value={isThemeDark}
+            onValueChange={toggleTheme}
+          />
+        </TouchableRipple>
+      </View>
     </ScreenContainer>
   );
 };
