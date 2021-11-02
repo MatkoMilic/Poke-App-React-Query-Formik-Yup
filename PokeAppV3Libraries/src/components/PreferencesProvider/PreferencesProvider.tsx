92c6d1d1e6a22ff3e83868fbe94910fc5de49b29
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-02 23:35:41 +0100

    feat(src/components/preferencesprovider/): add separate preferences provider

---
+++ PokeAppV3Libraries/src/components/PreferencesProvider/PreferencesProvider.tsx
@@ -0,0 +1,50 @@
+import React, {FC} from 'react';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+import {PreferencesContext} from '../PreferencesContext';
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
+interface PreferencesProviderProps {
+  children?: React.ReactNode;
+}
+
+const PreferencesProvider: FC<PreferencesProviderProps> = ({children}) => {
+  const [isThemeDark, setIsThemeDark] = React.useState(false);
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark],
+  );
+  return (
+    <PreferencesContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>{children}</NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
+  );
+};
+
+PreferencesProvider.defaultProps = {
+  children: undefined,
+};
+
+export default PreferencesProvider;
---
+++ PokeAppV3Libraries/src/components/PreferencesProvider/index.ts
@@ -0,0 +1,1 @@
+export {default as PreferencesProvider} from './PreferencesProvider';
