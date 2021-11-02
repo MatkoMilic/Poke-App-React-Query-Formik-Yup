4f91a70dfd047f2f45b93597c91e40bc42c13a44
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-02 17:33:55 +0100

    style(src/screens/settingsscreen/style.ts): add styles to style settings screen

---
+++ PokeAppV3Libraries/src/screens/SettingsScreen/style.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  switchView: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  switchElement: {
+    borderRadius: 15,
+  },
+  headerContent: {
+    alignItems: 'center',
+  },
+});
