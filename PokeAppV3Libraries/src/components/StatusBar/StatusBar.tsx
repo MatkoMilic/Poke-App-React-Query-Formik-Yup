0fe8ada445d5ac5e804fe5bfdb1b26a98f5d09c0
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-03 12:49:45 +0100

    refactor(statusbar.tsx): move unneeded export

--- PokeAppV3Libraries/src/components/StatusBar/StatusBar.tsx
+++ PokeAppV3Libraries/src/components/StatusBar/StatusBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {StatusBarProps, StatusBar as RnStatusBar} from 'react-native';
 import {PreferencesContext} from '../PreferencesContext';
 
-export interface IStatusBarProps extends StatusBarProps {}
+interface IStatusBarProps extends StatusBarProps {}
 
 const StatusBar: React.FC<IStatusBarProps> = ({...otherProps}) => {
   const {isThemeDark} = React.useContext(PreferencesContext);
