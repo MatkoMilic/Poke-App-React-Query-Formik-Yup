4932df0e0629655e759f36b23822d5edac350d67
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-03 17:43:36 +0100

    refactor(header.tsx): change header component logic, add more props to it, make it more reusable

--- PokeAppV3Libraries/src/components/Header/Header.tsx
+++ PokeAppV3Libraries/src/components/Header/Header.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {Appbar as RNPaperAppbar} from 'react-native-paper';
+import {IconSource} from 'react-native-paper/lib/typescript/components/Icon';
 import {styles} from './styles';
 
 type RNAppbarProps = React.ComponentProps<typeof RNPaperAppbar>;
-type IHeader = RNAppbarProps & {};
+type IHeader = RNAppbarProps & {
+  goToScreenLeftIcon?: () => void;
+  goToScreenRightIcon?: () => void;
+  headerTitle: string;
+  headerSubtitle?: string;
+  leftIcon: IconSource;
+  rightIcon: IconSource;
+  children?: ReactNode;
+};
 
-const Header: React.FC<IHeader> = ({...otherProps}) => {
-  return <RNPaperAppbar.Header style={styles.header} {...otherProps} />;
+const Header: React.FC<IHeader> = ({
+  goToScreenLeftIcon,
+  goToScreenRightIcon,
+  headerTitle,
+  headerSubtitle,
+  leftIcon,
+  rightIcon,
+  children,
+  ...otherProps
+}) => {
+  return (
+    <RNPaperAppbar.Header style={styles.header} {...otherProps}>
+      <RNPaperAppbar.Action
+        icon={leftIcon}
+        onPress={goToScreenLeftIcon}
+        size={30}
+      />
+      <RNPaperAppbar.Content
+        style={styles.headerContent}
+        title={headerTitle}
+        subtitle={headerSubtitle}
+      />
+      <RNPaperAppbar.Action
+        icon={rightIcon}
+        onPress={goToScreenRightIcon}
+        size={30}
+      />
+    </RNPaperAppbar.Header>
+  );
 };
 
 export default Header;
