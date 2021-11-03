b0dd6d2c8e2378317025d3d27698ff800050aa8f
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-03 13:33:26 +0100

    style(header.tsx & header/styles.ts): add styles to the header and change return item

--- PokeAppV3Libraries/src/components/Header/Header.tsx
+++ PokeAppV3Libraries/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Appbar, Appbar as RNPaperAppbar} from 'react-native-paper';
+import {Appbar as RNPaperAppbar} from 'react-native-paper';
+import {styles} from './styles';
 
 type RNAppbarProps = React.ComponentProps<typeof RNPaperAppbar>;
 type IHeader = RNAppbarProps & {};
 
 const Header: React.FC<IHeader> = ({...otherProps}) => {
-  return <Appbar.Header {...otherProps} />;
+  return <RNPaperAppbar.Header style={styles.header} {...otherProps} />;
 };
 
 export default Header;
