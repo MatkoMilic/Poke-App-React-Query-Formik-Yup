3a5eca7b617336de947521601450993264920990
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-05 12:43:35 +0100

    refactor(navigatornames navigatortypes screennames): rename these files so its no longer just index

---
+++ PokeAppV3Libraries/src/constants/screenNames/screenNames.ts
@@ -0,0 +1,10 @@
+import {
+  MainStackNameType,
+  OnboardingStackNameType,
+} from '../navigatorTypes/navigatorTypes';
+
+export const PROFILE_SCREEN: MainStackNameType = 'ProfileScreen';
+export const SETTINGS_SCREEN: MainStackNameType = 'SettingsScreen';
+export const POKELIST_SCREEN: MainStackNameType = 'PokeListScreen';
+export const LOADING_SCREEN: OnboardingStackNameType = 'LoadingScreen';
+export const LOGIN_SCREEN: OnboardingStackNameType = 'LoginScreen';
