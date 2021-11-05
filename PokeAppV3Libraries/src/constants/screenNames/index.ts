3c4f9d57db7fb4532bae547f11adc9bcec4b6c20
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-05 13:01:21 +0100

    refactor: remove deleted indexes due to renaming them and remove loginSchema due to moving it

--- PokeAppV3Libraries/src/constants/screenNames/index.ts
+++
@@ -1,7 +0,0 @@
-import {MainStackNameType, OnboardingStackNameType} from '../navigatorTypes';
-
-export const PROFILE_SCREEN: MainStackNameType = 'ProfileScreen';
-export const SETTINGS_SCREEN: MainStackNameType = 'SettingsScreen';
-export const POKELIST_SCREEN: MainStackNameType = 'PokeListScreen';
-export const LOADING_SCREEN: OnboardingStackNameType = 'LoadingScreen';
-export const LOGIN_SCREEN: OnboardingStackNameType = 'LoginScreen';
